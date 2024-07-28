7f650561cbbb6d650db48939fa27626c18cc0f17
reiiwang <sqq9458@gmail.com>
2024-07-29 01:26:46 +0800

    first commit

---
+++ data/question.py
@@ -0,0 +1,244 @@
+Contexts = [
+    {
+        # 17,
+        "Question": "What causes fibromyalgia? 纖維肌痛的原因是什麼？",
+        "Evidence": [
+            "1: We do know certain things suggest you're more likely to get it: You're a woman. 我們確實知道某些事情表明您更有可能得到它：您是女性。",
+            "2: You have another painful disease, such as arthritis, or an infection. 您患有另一種痛苦的疾病，例如關節炎或感染。",
+            "3: You have a mood disorder, like anxiety or depression. 您有情緒障礙，例如焦慮或憂鬱。",
+            "4: You were physically or emotionally abused or have PTSD. 您受到身體或情緒虐待或創傷後壓力症候群 (PTSD)。",
+            "5: You rarely exercise. 你很少運動。",
+            "6: Other family members have it. 其他家庭成員也有。",
+            "7: If your doctor can't find another reason for how you feel, they'll use a two-part scoring system to measure how widespread your pain has been and how much your symptoms affect your daily life. 如果您的醫生找不到您的感受的其他原因，他們將使用兩部分評分系統來衡量您的疼痛有多廣泛以及您的症狀對您日常生活的影響程度。"
+        ],
+        "Answer": "There is no known single cause of fibromyalgia. Factors that may increase the risk of developing fibromyalgia include: being a woman, having another painful condition, having a mood disorder, experiencing physical or emotional trauma, being sedentary, and having a family history of the condition. 纖維肌痛症沒有已知的單一原因。可能增加纖維肌痛風險的因素包括：女性、患有另一種痛苦的疾病、情緒障礙、經歷身體或情緒創傷、久坐以及有疾病的家族史。"
+    },
+    {
+        # 19,
+        "Question": "What are psychological ways that multiple sclerosis tremors can affect someone? 多發性硬化症震顫可以透過哪些心理方式影響某人？",
+        "Evidence": [
+            "1: Tremors can be tough to handle in social situations. 在社交場合顫抖可能很難處理。",
+            "2: You may feel like you want to be alone, but that can make you feel lonely and depressed. 您可能覺得自己想獨處，但這會讓您感到孤獨和沮喪。"
+        ],
+        "Answer": "Multiple sclerosis tremors can make it difficult to interact with others, which can lead to loneliness and depression. 多發性硬化症震顫會使人難以與他人互動，進而導致孤獨和憂鬱。"
+    },
+    {
+        # 442,
+        "Question": "Do certain foods cause eczema? 某些食物會引起濕疹嗎？",
+        "Evidence": [
+            "1. The common offenders are: Milk Eggs Peanuts Tree nuts Wheat Fish Shellfish Soy While trigger foods can make eczema worse, experts don't think they're really the original cause. 常見的罪魁禍首是： 牛奶 雞蛋 花生 堅果 小麥 魚類 貝類 大豆 雖然觸發食物會使濕疹惡化，專家認為它們並不是真正的原始原因。",
+            "2. Instead, it seems to result from \"leakiness\" in the outer layer of skin that lets in irritants, germs, and allergens. 相反，它似乎是由於皮膚外層的「滲漏」導致刺激物、細菌和過敏原進入。"
+        ],
+        "Answer": "Milk, eggs, peanuts, tree nuts, wheat, fish, shellfish, and soy are common food triggers for eczema. However, experts believe that these foods are not the root cause of eczema. Instead, they believe that eczema is caused by a \"leakiness\" in the outer layer of skin that allows irritants, germs, and allergens to enter the body. 牛奶、雞蛋、花生、堅果、小麥、魚、貝類和大豆是濕疹的常見食物誘因。然而專家認為，這些食物並不是濕疹的根本原因。相反，他們認為濕疹是由皮膚外層的「滲漏」引起的，刺激物、細菌和過敏原可以進入體內。"
+    },
+    {
+        # 444,
+        "Question": "How can reality orientation training help with treating dementia? 現實導向訓練如何幫助治療癡呆症？",
+        "Evidence": [
+            "1. The person leading the session might use music from your loved one's past, or things like photos or treasured items, to help. 主持會議的人可能會使用您所愛的人過去的音樂，或者照片或珍貴物品等東西來提供幫助。",
+            "2. They might have signs with that information placed around their home. 他們的家周圍可能會放置帶有這些訊息的標誌。",
+            "3. Some people find this to be too much or even patronizing. 有些人覺得太過分了，甚至是居高臨下。",
+            "4. If it's not working for your loved one, drop it. 如果它對你所愛的人不起作用，那就放棄它。"
+        ],
+        "Answer": "Reality orientation training can help with treating dementia by using music, photos, or treasured items from the past to help remind the person of who they are and where they are. It can also involve placing signs with that information around the home. However, some people find this to be too much or even patronizing, so it is important to listen to your loved one and see what works for them. 現實定向訓練可以透過使用音樂、照片或過去的珍貴物品來幫助提醒患者他們是誰以及他們在哪裡，從而幫助治療癡呆症。它還可能涉及在家中周圍放置帶有該訊息的標誌。然而，有些人覺得這太過分了，甚至有點居高臨下，所以傾聽你所愛的人的意見並看看什麼對他們有效很重要。"
+    },
+    {
+        # 4045,
+        "Question": "How much fiber should you have to treat constipation? 您應該攝取多少纖維來治療便秘？",
+        "Evidence": [
+            "1. Gradually increase the amount of fiber in your diet until you're getting at least 20 to 35 grams of fiber daily. 逐漸增加飲食中的纖維含量，直到每天至少攝取 20 至 35 克纖維。",
+            "2. Moving your body will keep your bowels moving, too. 移動你的身體也會讓你的腸道保持蠕動。"
+        ],
+        "Answer": "To treat constipation, gradually increase the amount of fiber in your diet until you're getting at least 20 to 35 grams of fiber daily. Additionally, exercise can help keep your bowels moving. 要治療便秘，請逐漸增加飲食中的纖維含量，直到每天至少攝取 20 至 35 克纖維。此外，運動可以幫助保持腸道蠕動。"
+    },
+    {
+        # 2047,
+        "Question": "How can a walking aid help with knee pain? 助行器如何幫助緩解膝蓋疼痛？",
+        "Evidence": [
+            "1. A crutch or cane can take the stress off of your knee. 拐杖或手杖可以減輕膝蓋的壓力。",
+            "2. Knee splints and braces can also help you stay stable. 膝蓋夾板和支架也可以幫助您保持穩定。"
+        ],
+        "Answer": "Crutches and canes can help relieve knee pain by taking the stress off of the knee. Knee splints and braces can also help stabilize the knee and reduce pain. 拐杖和手杖可以減輕膝蓋的壓力，從而緩解膝蓋疼痛。膝蓋夾板和支架還可以幫助穩定膝蓋並減輕疼痛。"
+    },
+    {
+        # 51,
+        "Question": "Should I discuss natural treatments for migraines with my doctor? 我應該與我的醫生討論偏頭痛的自然療法嗎？",
+        "Evidence": [
+            "1: Remember, all natural doesn't necessarily mean safe. 請記住，「自然」並不一定意味著安全。",
+            "2: Any supplement you take should be discussed with your doctor. 您服用的任何補充劑都應與您的醫生討論。"
+        ],
+        "Answer": "It is important to discuss natural treatments for migraines with your doctor, as some of them may not be safe. Additionally, your doctor can help you determine if a particular supplement is right for you. 與您的醫生討論偏頭痛的自然療法很重要，因為其中一些可能不安全。此外，您的醫生可以幫助您確定特定的補充劑是否適合您。"
+    },
+    {
+        # 45,
+        "Question": "What lifestyle changes can you make to treat dilated cardiomyopathy (DCM)? 您可以改變哪些生活方式來治療擴張型心肌病變 (DCM)？",
+        "Evidence": [
+            "1: Lifestyle changes: If you have heart failure, you should have less sodium, based on your doctor's recommendations. 生活方式的改變：如果您患有心臟衰竭，您應該根據醫生的建議減少鈉的攝取。",
+            "2: He may point you toward aerobic exercise, but don't do heavy weightlifting. 他可能會建議你進行有氧運動，但不要做大舉重。"
+        ],
+        "Answer": "Lifestyle changes that can be made to treat dilated cardiomyopathy (DCM) include reducing sodium intake and engaging in aerobic exercise. However, heavy weightlifting should be avoided. 可以透過改變生活方式來治療擴張型心肌病變 (DCM)，包括減少鈉攝取量和進行有氧運動。但應避免舉重。"
+    },
+    {
+        # 39,
+        "Question": "How can regular exercise help prevent weight gain during menopause? 規律運動如何幫助預防更年期體重增加？",
+        "Evidence": [
+            "1. Here's what helps: Eat a bit less. 以下是有幫助的：少吃一點。",
+            "2. Maintain muscle. 維持肌肉。",
+            "3. Walking, jogging, and strength training also help you shed pounds. 步行、慢跑和肌力訓練也能幫助你減重。"
+        ],
+        "Answer": "Regular exercise can help prevent weight gain during menopause by helping you to eat a bit less, maintain muscle, and engage in activities like walking, jogging, and strength training. 規律運動可以幫助您少吃一點、保持肌肉、步行、慢跑和肌力訓練，有助於防止更年期體重增加。"
+    },
+    {
+        # 29,
+        "Question": "How can I avoid indoor allergens to ease eczema symptoms? 如何避免室內過敏原以緩解濕疹症狀？",
+        "Evidence": [
+            "1. Sweating dries out your skin, and the salt in sweat can sting and irritate it. 出汗會使皮膚乾燥，汗水中的鹽分會刺痛和刺激皮膚。",
+            "2. Wear layers, and take them off and put them on as needed to stay comfortable. 多穿幾層衣服，視需要脫下來再穿上，以保持舒適。"
+        ],
+        "Answer": "To avoid indoor allergens to ease eczema symptoms, avoid sweating and wear layers of clothing that can be removed as needed. 為了避免室內過敏原以緩解濕疹症狀，請避免出汗並穿上可根據需要脫掉的多層衣服。"
+    },
+    {
+        # 20,
+        "Question": "Are there medication side effects for treating rheumatoid arthritis? 治療類風濕性關節炎的藥物有副作用嗎？",
+        "Evidence": [
+            "1. The skin problems people with RA have are often related to the medications they take to ease symptoms or control their disease. RA 患者的皮膚問題通常與他們為緩解症狀或控制疾病而服用的藥物有關。",
+            "2. Drug-related skin problems include: Skin rashes. 與藥物有關的皮膚問題包括：皮疹。"
+        ],
+        "Answer": "Medications used to treat rheumatoid arthritis can cause skin problems. These include skin rashes. 用於治療類風濕性關節炎的藥物可能會導致皮膚問題。這些包括皮疹。"
+    },
+    {
+        # 34,
+        "Question": "How is two minute relaxation used to manage stress from multiple sclerosis? 如何使用兩分鐘的放鬆來控制多發性硬化症帶來的壓力？",
+        "Evidence": [
+            "1. Switch your thoughts to yourself and your breathing. 將你的思想轉向你自己和你的呼吸。",
+            "2. Take a few deep breaths, then breathe out slowly. 深呼吸幾次，然後慢慢吐氣。",
+            "3. Mentally scan your body. 精神掃描你的身體。",
+            "4. Notice areas that feel tense or cramped and then loosen them up. 注意感覺緊張或狹窄的部位，然後放鬆它們。"
+        ],
+        "Answer": "To manage stress from multiple sclerosis, two minute relaxation can be used by: focusing on breathing, scanning the body for tense areas, and loosening them up. 為了控制多發性硬化症帶來的壓力，可以透過以下方式進行兩分鐘的放鬆：專注於呼吸，掃描身體的緊張區域，然後放鬆它們。"
+    },
+    {
+        # 2004,
+        "Question": "How can iron help rebuild your bone after a fracture? 鐵如何幫助骨折後重建骨骼？",
+        "Evidence": [
+            "1. When you have a fracture, your body needs it to build new bone for the repair. 當您骨折時，您的身體需要它來建造新骨骼以進行修復。",
+            "2. Iron helps your body make collagen to rebuild bone. 鐵可以幫助您的身體產生膠原蛋白來重建骨骼。",
+            "3. It also plays a part in getting oxygen into your bones to help them heal. 它還有助於讓氧氣進入骨骼，幫助骨骼癒合。",
+            "4. Good sources: Red meat, dark-meat chicken or turkey, oily fish, eggs, dried fruits, leafy green veggies, whole-grain breads, and fortified cereals. 良好來源：紅肉、黑肉雞肉或火雞、油性魚、雞蛋、乾果、綠葉蔬菜、全麥麵包和強化穀物。"
+        ],
+        "Answer": "Iron is essential for building new bone tissue after a fracture. It helps your body make collagen, which is a protein that is necessary for bone formation. Iron also plays a role in transporting oxygen to your bones, which is important for healing. Good sources of iron include red meat, dark-meat chicken or turkey, oily fish, eggs, dried fruits, leafy green vegetables, whole-grain breads, and fortified cereals. 鐵對於骨折後建造新骨組織至關重要。它可以幫助您的身體產生膠原蛋白，這是骨骼形成所必需的蛋白質。鐵還可以將氧氣輸送到骨骼，這對癒合很重要。鐵的良好來源包括紅肉、黑肉雞肉或火雞、油性魚、雞蛋、乾果、綠葉蔬菜、全麥麵包和強化穀物。"
+    },
+    {
+        # 410,
+        "Question": "Are over-the-counter wart removers safe? 非處方除疣劑安全嗎？",
+        "Evidence": [
+            "1. Most dermatologists say it's safe to try drugstore wart removers -- as long as you've confirmed that it's really a wart. 大多數皮膚科醫生說，嘗試藥房的除疣劑是安全的——只要你確認它確實是疣。",
+            "2. Sometimes calluses or corns are mistaken for warts. 有時老繭或雞眼會被誤認為是疣。",
+            "3. If in doubt, ask your doctor. 如果有疑問，請問你的醫生。"
+        ],
+        "Answer": "According to most dermatologists, over-the-counter wart removers are safe to use as long as you are certain that the growth is a wart. However, it is important to note that calluses and corns can sometimes be mistaken for warts, so if you are unsure, it is best to consult with a doctor. 根據大多數皮膚科醫生的說法，只要您確定生長物是疣，非處方疣去除劑就可以安全使用。不過，需要注意的是，老繭和雞眼有時會被誤認為是疣，因此，如果您不確定，最好諮詢醫生。"
+    },
+    {
+        # 1038,
+        "Question": "Can stress cause high blood sugar levels? 壓力會導致高血糖嗎？",
+        "Evidence": [
+            "1. Stress blocks your body from releasing insulin, and that lets glucose pile up in your blood. 壓力會阻礙身體釋放胰島素，進而導致血液中的葡萄糖堆積。",
+            "2. If you're stressed for a long time, your sugar levels will keep building. 如果您長期承受壓力，您的血糖值會不斷升高。",
+            "3. Regular exercise and relaxation techniques -- like yoga, meditation, tai chi, and breathing exercises -- can help. 定期運動和放鬆技巧——如瑜伽、冥想、太極拳和呼吸練習——會有幫助。"
+        ],
+        "Answer": "Stress can cause high blood sugar levels by blocking the body from releasing insulin, which allows glucose to build up in the blood. Over time, this can lead to serious health problems. Regular exercise and relaxation techniques can help to manage stress and keep blood sugar levels in check. 壓力會阻止身體釋放胰島素，導致血液中葡萄糖積聚，導致高血糖。隨著時間的推移，這可能會導致嚴重的健康問題。定期運動和放鬆技巧可以幫助控制壓力並控制血糖水平。"
+    },
+    {
+        # 33,
+        "Question": "What are some ways to avoid germs while I'm being treated for cancer? 在我接受癌症治療期間，有哪些方法可以避免細菌？",
+        "Evidence": [
+            "1. Be careful around people. 小心周圍的人。",
+            "2. If you know that someone has a fever, cold, flu, or any other infection, stay away from them until they feel better. 如果您知道有人發燒、感冒、流感或任何其他感染，請遠離他們，直到他們感覺好轉。",
+            "3. It's also best to skip large groups of people, so avoid shopping, schools, traveling, and crowds. 最好避開一大群人，避免購物、上學、旅行和人群。",
+            "4. If you do go out in public, use cleaning wipes before you touch door handles, elevator buttons, ATM keypads, or any other surface used by a lot of people. 如果您確實去公共場所，請在觸摸門把手、電梯按鈕、ATM 鍵盤或任何其他許多人使用的表面之前使用清潔濕巾。"
+        ],
+        "Answer": "To avoid germs while being treated for cancer, be careful around people, avoid those who are sick, skip large groups, and use cleaning wipes when touching public surfaces. 為了在接受癌症治療時避免細菌，請小心周圍的人，避開生病的人，避免人群聚集，並在接觸公共表面時使用清潔濕巾。"
+    },
+    {
+        # 24,
+        "Question": "What are some common triggers of chronic idiopathic urticaria (hives)? 慢性特發性蕁麻疹（蕁麻疹）的常見誘因有哪些？",
+        "Evidence": [
+            "1: Even though doctors can't say for sure what causes CIU, they do know things that can lead to flare-ups. 儘管醫生不能確定導致 CIU 的原因，但他們確實知道可能導致病情發作的因素。",
+            "2: They include: Alcoholic drinks Tight clothing Non-steroidal anti-inflammatory drugs (NSAIDS) like aspirin and ibuprofen Exercise Cold Heat If you've had hives for several weeks and don't know why, see your doctor. 包括： 酒精飲料 緊身衣服 非類固醇抗發炎藥物 (NSAIDS)，如阿斯匹靈和布洛芬 運動 冷熱 如果您出現蕁麻疹數週且不知道原因，請去看醫生。"
+        ],
+        "Answer": "Although doctors cannot pinpoint the exact cause of chronic idiopathic urticaria (hives), they have identified a number of potential triggers, including alcoholic drinks, tight clothing, non-steroidal anti-inflammatory drugs (NSAIDs) like aspirin and ibuprofen, exercise, cold, and heat. 雖然醫生無法找出慢性特發性蕁麻疹（蕁麻疹）的確切原因，但他們已經確定了許多潛在的誘因，包括酒精飲料、緊身衣服、阿斯匹靈等非類固醇抗發炎藥(NSAID) 和布洛芬、運動、冷和熱。"
+    },
+    {
+        # 487,
+        "Question": "Does prickly pear cactus affect other medications? 仙人掌會影響其他藥物嗎？",
+        "Evidence": [
+            "1. If you take any medications regularly, talk to your doctor before you start using prickly pear cactus supplements. 如果您定期服用任何藥物，請在開始使用仙人掌補充劑之前諮詢您的醫生。",
+            "2. Since prickly pear cactus affects blood sugar levels, it may not be safe if you're taking diabetes drugs. 由於仙人掌會影響血糖水平，因此如果您正在服用糖尿病藥物，它可能不安全。"
+        ],
+        "Answer": "Individuals taking medications should consult with a doctor before using prickly pear cactus supplements. Prickly pear cactus may interact with diabetes drugs, as it affects blood sugar levels. 服用藥物的個人在使用仙人掌補充劑之前應諮詢醫生。仙人掌可能與糖尿病藥物產生交互作用，因為它會影響血糖水平。"
+    },
+    {
+        # 53,
+        "Question": "Can eating breakfast help with bad breath? 吃早餐可以治療口臭嗎？",
+        "Evidence": [
+            "1: Eat breakfast: Even if you brush your teeth when you get up, your morning breath may reappear if you don't eat. 吃早餐：即使起床刷牙，如果不吃東西，早上的口氣也可能會重新出現。",
+            "2: Morning mouth may be associated with hunger. 早上張嘴可能與飢餓有關。"
+        ],
+        "Answer": "Eating breakfast can help with bad breath because it reduces hunger, which is associated with morning breath. 吃早餐可以緩解口臭，因為它可以減少飢餓感，而飢餓感與早晨的口氣有關。"
+    },
+    {
+        # 1033,
+        "Question": "How does amylin analog treat type 2 diabetes? 胰淀素類似物如何治療第 2 型糖尿病？",
+        "Evidence": [
+            "1. Amylin analog: This type slows your digestion. 胰淀素類似物：這種類型會減慢您的消化速度。",
+            "2. That keeps your glucose level down and helps your liver leave the numbers low. 這可以降低你的血糖水平，並幫助你的肝臟保持較低的血糖水平。",
+            "3. You inject these before meals. 您可以在飯前註射這些藥物。"
+        ],
+        "Answer": "Amylin analog is a type of medication that helps to slow down digestion. This helps to keep glucose levels down and helps the liver to keep levels low. Amylin analogs are injected before meals. 胰淀素類似物是一種有助於減緩消化的藥物。這有助於降低血糖水平並幫助肝臟保持在較低水平。胰淀素類似物在飯前註射。"
+    }
+]
+
+questions = [
+    {
+        "text": "Question 1: 答案(Answer)是否只包含證據(Evidence)中提到的資訊? (亦即是否有超出證據的資訊)",
+        "options": ["1: 完全符合", "2: 大部分符合", "3: 少部分符合", "4: 完全不符合"],
+        "options_note": ["完全符合 (答案完全依循證據中的資訊，沒有任何超出證據的額外資訊。換言之，答案提及的資訊都可以在證據中找到)。\
+                         [補充: 這包含用詞不同但意義上相同的答案，或者為了使回答更流暢而加入的詞句並不算超出證據]",
+                         "大部分符合 (答案大部分依循證據中的資訊，但可能加入了一些輔助資訊或解釋。\
+                            這些輔助資訊並不改變證據的主要含義，但可能增加了一些背景或細節，並且這些細節是正確的。)",
+                         "部分符合 (答案只有部分根據證據中的資訊，其他大部分內容是由 LLM 加入的額外資訊或解釋，這些資訊可能是證據的延伸或無關，\
+                            但無法保證這些延伸的正確性)",
+                         "完全不符合 (答案完全沒有根據證據中的資訊，內容都是額外資訊、錯誤資訊或與證據無關的內容。)"]
+    },
+    {
+        "text": "Question 2: 答案(Answer)是否涵蓋所有證據(Evidence)中提及的資訊?",
+        "options": ["1: 完全涵蓋","2: 大部分涵蓋","3: 部分涵蓋", "4: 完全未涵蓋"],
+        "options_note": ["完全涵蓋 (答案的內容涵蓋了證據中每個提及的重要資訊，這些資訊在答案中可能經過重寫、精簡、總結，但意思不變。)",
+                            "大部分涵蓋 (答案內容涵蓋證據中的大部分片段的重要資訊，\
+                                但可能有些未包含，其中未包含的片段為可省略的次要或無關資訊。)",
+                            "部分涵蓋 (答案中只有一部分重要資訊被涵蓋，大部分證據片段並未在答案被提及，代表有重要的片段被忽略了。)",
+                            "完全未涵蓋 (答案中完全沒有涵蓋證據中的任何句子所提及的重要資訊。)"]
+    },
+    {
+        "text": "Question 3: 答案(Answer)的表達是否通順且流利?",
+        "options": ["1: 非常通順","2: 較為通順","3: 不太通順", "4: 非常不通順"],
+        "options_note": ["非常通順 (答案表達流暢、通順。)",
+                            "較為通順 (答案表達大致流暢，但可能有少量語法錯誤或不自然的表達方式，但不影響整體理解。)",
+                            "不太通順 (答案表達不太流暢，有較多的語法錯誤或不自然的地方，影響了理解。)",
+                            "非常不通順 (答案表達不流暢，具有語法錯誤與不自然的表達，難以理解。)"]
+    },
+    {
+        "text": "Question 4: 答案 (Answer) 的內容是否正確?",
+        "options": ["1: 完全正確","2: 大部分正確","3: 部分正確", "4: 完全不正確"],
+        "options_note": ["完全正確 (獨立於其他題目，假設Evidence為Ground truth (正解) 的情況下，答案中的資訊正確，與證據或已知事實一致。或者基於你的知識背景，答案中的資訊是正確的。)",
+                         "大部分正確 (獨立於其他題目，假設Evidence為Ground truth (正解) 的情況下，答案中的大部分資訊是正確的，但可能有一些小錯誤或不準確的地方。)",
+                         "部分正確 (獨立於其他題目，假設Evidence為Ground truth (正解) 的情況下，答案中只有一部分資訊是正確的，大部分內容有誤。)",
+                         "完全不正確 (獨立於其他題目，假設Evidence為Ground truth (正解) 的情況下，答案中的所有資訊都是錯誤的，沒有一部分是正確的。)"]
+    }
+]
+
+def get_contexts():
+    return Contexts
+
+def get_questions():
+    return questions
